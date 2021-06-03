9de65815c3ebb66728acd1d343e136a5d33c3ca3
Nicole Hall <nicolealexandrahall@gmail.com>
2021-06-03 16:42:32 -0600

    adds button. when clicked it sends an alert that says hi. tests pass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2020%2F06%2F26%2Ftiny-white-kitten-873941684-2000.jpg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  onButtonClickHandler = () => {
+    window.alert('Hi')
+  }; 
+
+  render() {
+    return (
+      <div className="App">
+        <img className="App-logo" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2020%2F06%2F26%2Ftiny-white-kitten-873941684-2000.jpg" className="App-logo" alt="logo" />
+        <br></br>
+        <button onClick={this.onButtonClickHandler}>Hello world!</button>
+      </div>
+    );
+  }
 }
-
 export default App;
+
+
+
+
+
